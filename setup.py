eea90c4b7c7619fb97f4ead3993f3d81544b76d3
Vasily Stepanov <vasily.stepanov@gmail.com>
2022-02-19 13:59:20 +0300

    Add package metadata.

--- setup.py
+++ setup.py
@@ -1,14 +1,36 @@
+import os
 from setuptools import setup
+import efidgy
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
     name='efidgy',
-    version='0.11',
+    version=efidgy.__version__,
+    author='Vasily Stepanov',
+    author_email='vasily.stepanov@efidgy.com',
+    license='MIT',  # FIXME
+    keywords='',  # FIXME
+    url='https://github.com/efidgy/efidgy',
+    long_description=read('README.md'),
+    sifiers=[  # FIXME
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development :: Code Generators',
+        'License :: OSI Approved :: MIT License',
+    ],
     packages=[
         'efidgy',
         'tests'
     ],
     install_requires=[
-        'httpx==0.22',
+        'httpx>=0.22',
     ],
+    # entry_points = {
+    #     'console_scripts' : [
+    #         'efidgy = efidgy:main'
+    #     ],
+    # },
 )
